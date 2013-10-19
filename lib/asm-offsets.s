ccfe5c79ec9c06e899d4b1413ffbe257b8ba09d4
Eugene Agafonov <e.a.agafonov@gmail.com>
2013-10-19 13:19:26 +0400

    Wipe build artifacts from SCM

--- lib/asm-offsets.s
+++ lib/asm-offsets.s
@@ -26,6 +26,7 @@ main:
 	@ frame_needed = 0, uses_anonymous_args = 0
 	@ link register save eliminated.
 	.loc 1 25 0
+#APP
 @ 25 "lib/asm-offsets.c" 1
 	
 ->GENERATED_GBL_DATA_SIZE #128 (sizeof(struct global_data)+15) & ~15
@@ -40,18 +41,18 @@ main:
 .Letext0:
 	.section	.debug_info,"",%progbits
 .Ldebug_info0:
-	.4byte	0x93
+	.4byte	0x8b
 	.2byte	0x2
 	.4byte	.Ldebug_abbrev0
 	.byte	0x4
 	.uleb128 0x1
-	.4byte	.LASF11
+	.4byte	.LASF10
 	.byte	0x1
+	.4byte	.LASF11
 	.4byte	.LASF12
-	.4byte	.LASF13
+	.4byte	0
+	.4byte	0
 	.4byte	.Ldebug_ranges0+0
-	.4byte	0
-	.4byte	0
 	.4byte	.Ldebug_line0
 	.uleb128 0x2
 	.byte	0x1
@@ -91,19 +92,15 @@ main:
 	.4byte	.LASF7
 	.uleb128 0x2
 	.byte	0x4
-	.byte	0x7
-	.4byte	.LASF8
-	.uleb128 0x2
-	.byte	0x4
 	.byte	0x5
-	.4byte	.LASF9
+	.4byte	.LASF8
 	.uleb128 0x2
 	.byte	0x1
 	.byte	0x8
-	.4byte	.LASF10
+	.4byte	.LASF9
 	.uleb128 0x4
 	.byte	0x1
-	.4byte	.LASF14
+	.4byte	.LASF13
 	.byte	0x1
 	.byte	0x16
 	.byte	0x1
@@ -113,7 +110,6 @@ main:
 	.byte	0x2
 	.byte	0x7d
 	.sleb128 0
-	.byte	0x1
 	.byte	0
 	.section	.debug_abbrev,"",%progbits
 .Ldebug_abbrev0:
@@ -128,12 +124,12 @@ main:
 	.uleb128 0xe
 	.uleb128 0x1b
 	.uleb128 0xe
-	.uleb128 0x55
-	.uleb128 0x6
 	.uleb128 0x11
 	.uleb128 0x1
 	.uleb128 0x52
 	.uleb128 0x1
+	.uleb128 0x55
+	.uleb128 0x6
 	.uleb128 0x10
 	.uleb128 0x6
 	.byte	0
@@ -181,8 +177,6 @@ main:
 	.uleb128 0x1
 	.uleb128 0x40
 	.uleb128 0xa
-	.uleb128 0x2117
-	.uleb128 0xc
 	.byte	0
 	.byte	0
 	.byte	0
@@ -207,34 +201,34 @@ main:
 	.section	.debug_line,"",%progbits
 .Ldebug_line0:
 	.section	.debug_str,"MS",%progbits,1
-.LASF6:
-	.ascii	"long long int\000"
 .LASF5:
 	.ascii	"unsigned int\000"
-.LASF14:
-	.ascii	"main\000"
-.LASF11:
-	.ascii	"GNU C 4.7.3\000"
 .LASF1:
 	.ascii	"long unsigned int\000"
-.LASF7:
-	.ascii	"long long unsigned int\000"
-.LASF0:
-	.ascii	"unsigned char\000"
-.LASF12:
-	.ascii	"lib/asm-offsets.c\000"
-.LASF13:
-	.ascii	"/home/cmp/v2r_kernel/uboot\000"
-.LASF9:
-	.ascii	"long int\000"
-.LASF2:
-	.ascii	"short unsigned int\000"
 .LASF3:
 	.ascii	"signed char\000"
+.LASF11:
+	.ascii	"lib/asm-offsets.c\000"
+.LASF13:
+	.ascii	"main\000"
+.LASF7:
+	.ascii	"long long unsigned int\000"
+.LASF10:
+	.ascii	"GNU C 4.6.3\000"
+.LASF0:
+	.ascii	"unsigned char\000"
 .LASF8:
-	.ascii	"sizetype\000"
+	.ascii	"long int\000"
 .LASF4:
 	.ascii	"short int\000"
-.LASF10:
+.LASF12:
+	.ascii	"/home/pieslice/projects/virt2real/virt2real-sdk/ubo"
+	.ascii	"ot\000"
+.LASF9:
 	.ascii	"char\000"
-	.ident	"GCC: (Sourcery CodeBench Lite 2013.05-23) 4.7.3"
+.LASF2:
+	.ascii	"short unsigned int\000"
+.LASF6:
+	.ascii	"long long int\000"
+	.ident	"GCC: (Sourcery CodeBench Lite 2012.03-57) 4.6.3"
+	.section	.note.GNU-stack,"",%progbits
